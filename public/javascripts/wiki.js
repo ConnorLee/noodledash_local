c9762bfceb86693366869ed5ec80ac5b108da3a7
jeffschwartz <jefftschwartz@gmail.com>
2013-05-18 16:39:07 -0400

    Added delete confirmation with bootstrap modal and fixed a bug that was cauisng ghost events on delete

--- public/javascripts/wiki.js
+++ public/javascripts/wiki.js
@@ -2,29 +2,35 @@
 if ( $( '#wiki' ).length ) {
 
     $( function () {
-        var $wikiArticleEditBtn = $( '.wiki-article-edit-btn' ),
+        var /*$wikiArticleEditBtn = $( '.wiki-article-edit-btn' ),*/
             $wikiArticleDeleteBtn = $( '.wiki-article-delete-btn' );
 
-        $wikiArticleEditBtn.click( function ( event ) {
+        /*
+        $wikiArticleEditBtn.on( 'click', function ( event ) {
 
             var $article = $( this ).parents( '.article' );
 
             alert( $article.attr( 'data-id' ) );
         } );
+        */
 
-        $wikiArticleDeleteBtn.click( function ( event ) {
-            var response = confirm( "Deletetion are permanent and cannot be undone. " +
-                "Are you sure you want to delete this article?" );
+        $wikiArticleDeleteBtn.off('click').on( 'click', function ( event ) {
             var $article = $( this ).parents( '.article' );
             var articleId = $( this ).parents( '.article' ).attr( 'data-id' );
             var promise;
             var $modalArticleDeleted = $( '#modalDeletedArticle' );
             var $modalArticleDoesNotExist = $( '#modalArticleDoesNotExist' );
+            //var response = confirm( "Deletetion are permanent and cannot be undone. " +
+            //    "Are you sure you want to delete this article?" );
+
+            console.log('request to delete article ', articleId);
+
+            $( '#modalConfirmDelete' ).modal();
 
             // testing deletion of a non existant article
             //articleId = '5197be49c5cd1c470d000005';
 
-            if ( response ) {
+            $( '#wiki-confirm-delete-article-btn' ).off('click').on( 'click', function ( event ) {
                 ///wiki/api/v1/article/:articleid - end point url for ajax call
                 promise = $.ajax( {
                     url  : '/wiki/api/v1/article/' + articleId,
@@ -37,20 +43,20 @@ if ( $( '#wiki' ).length ) {
                         if ( json.response === 'deleted' ) {
                             //alert( 'The article has been deleted.' );
                             $modalArticleDeleted.modal();
-                            $modalArticleDeleted.on('hidden', function(){
+                            $modalArticleDeleted.on( 'hidden', function () {
                                 $article.hide( 'slow', function () {
                                     $article.remove();
                                 } );
-                            });
+                            } );
                         } else {
                             // already deleted and doesn't exist in the db so remove it from the dom
                             //alert( "This article no longer exists!" );
                             $modalArticleDoesNotExist.modal();
-                            $modalArticleDoesNotExist.on('hidden', function(){
+                            $modalArticleDoesNotExist.on( 'hidden', function () {
                                 $article.hide( 'slow', function () {
                                     $article.remove();
                                 } );
-                            });
+                            } );
                         }
                     },
                     // rejected
@@ -58,7 +64,7 @@ if ( $( '#wiki' ).length ) {
                         alert( "There was a problem on the server and it was unable to delete this article!" )
                     }
                 );
-            }
+            } );
         } );
     } );
 }
