b1bdec74064e3d5fc871d83228caca9eb7c5722a
Connor Lee <connorlee57@gmail.com>
2014-02-26 16:45:39 -0500

    Responsive CSS changes

--- public/javascripts/menu-helper.js
+++ public/javascripts/menu-helper.js
@@ -20,4 +20,11 @@ $(function() {
   $(".fa.sub").click(function() {  
     $(this).addClass("active");   
   });
+
+ if(!$(this).hasClass('active')) {
+ menu_a.removeClass('active');
+    } else {
+//remove previous class that was toggled active
+ menu_a.removeClass('active');
+    }
 });
