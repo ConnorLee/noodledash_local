d3af21af7a6d4c7215564225e9ae0c6a21e124cc
Connor Lee <connorlee57@gmail.com>
2014-02-28 11:51:10 -0500

    Fixed JS function that was hiding fonts on load

--- public/javascripts/menu-helper.js
+++ public/javascripts/menu-helper.js
@@ -1,30 +1,29 @@
-$(function() {
-   	    var menu_ul = $('.menu > li:not("active") > ul'),
-   	           menu_a  = $('.menu > li > a');
-   	     menu_ul.hide();
-   	    menu_a.click(function(e) {
-   	        e.preventDefault();
-   	        if(!$(this).hasClass('active')) {
-   	            menu_a.removeClass('active');
-   	            menu_ul.filter(':visible').slideUp('normal');
-   	            $(this).addClass('active').next().stop(true,true).slideDown('normal');
-   	        } else {
-   	            $(this).removeClass('active');
-   	            $(this).next().stop(true,true).slideUp('normal');
-   	        }
-   	    });
-   	});
+$(function () {
+    var menu_ul = $('.menu > li:not("active") > ul'),
+        menu_a = $('.menu > li > a');
+    menu_ul.hide();
+    menu_a.click(function (e) {
+        e.preventDefault();
+        if (!$(this).hasClass('active')) {
+            menu_a.removeClass('active');
+            menu_ul.filter(':visible').slideUp('normal');
+            $(this).addClass('active').next().stop(true, true).slideDown('normal');
+        } else {
+            $(this).removeClass('active');
+            $(this).next().stop(true, true).slideUp('normal');
+        }
+    });
 
+});
 
-// $(function() {                       
-//   $(".fa.sub").click(function() {  
-//     $(this).addClass("active");   
-//   });
-
-// //  if(!$(this).hasClass('active')) {
-// //  menu_a.removeClass('active');
-// //     } else {
-// // //remove previous class that was toggled active
-// //  menu_a.removeClass('active');
-// //     }
-// // });
+$(function () {
+    $(".fa.sub").click(function () {
+        $(this).addClass("active");
+    });
+    if (!$(this).hasClass('active')) {
+        menu_a.removeClass('active');
+    } else {
+        //remove previous class that was toggled active
+        menu_a.removeClass('active');
+    }
+});
